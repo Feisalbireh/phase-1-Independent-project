4e2cdc2a5f9a2ed1a14befd1387cca66440b10ba
feisalbireh <feisal.bireh@student.moringaschool.com>
2022-12-15 23:00:57 +0300

    fetching the api

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const searchBtn = document.getElementById('search-btn')
-const mealOR = document.getElementById('meal')
+const mealOR = document.getElementById('recipe')
 const recipeContent = document.querySelector('recipe-content')
 const recipeShut = document.getElementById('recipe-shut-btn')
 
 searchBtn.addEventListener('click', getRecipeList)
+
+function getRecipeList(){
+    let searchInput = document.getElementById 
+    ('search-inp').value.trim()
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+
+}
