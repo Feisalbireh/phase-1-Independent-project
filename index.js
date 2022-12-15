63fc3eb0232dfb065e3776a076931e9d01d338ed
feisalbireh <feisal.bireh@student.moringaschool.com>
2022-12-15 23:10:16 +0300

    adding a scrollbar

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ function getRecipeList(){
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        let html = ""
+        if(data.meals){
+            data.meals.forEach(meal =>{
+                html += `<div class="recipe-item" data-id = "${meal.idMeal}">
+                        <div class="recipe-img">
+                            <img src="${meal.strMealThumb}" alt="lasagna">
+                        </div>
+                        <div class="recipe-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="ingredients-btn">Ingredients</a>
+                        </div>
+                    </div>`;
+            })
+        }
+        mealOR.innerHTML = html;
     })
 
 }
