b5e0db0584dc4267a8b368d9bac48fdf324377d8
feisalbireh <feisal.bireh@student.moringaschool.com>
2022-12-20 11:59:48 +0300

    adding modal

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
-const searchBtn = document.getElementById('search-btn')
-const mealOR = document.getElementById('recipe')
-const recipeContent = document.querySelector('recipe-content')
-const recipeShut = document.getElementById('recipe-shut-btn')
+const searchBtn = document.getElementById("search-btn");
+const mealOr = document.getElementById("recipe");
+const mealDetailsContent = document.querySelector(".recipe-content");
+const recipeShut = document.getElementById("recipe-shut-btn");
 
-searchBtn.addEventListener('click', getRecipeList)
-mealOR.addEventListener('click', getMealRecipe)
+searchBtn.addEventListener("click", getRecipeList);
+mealOr.addEventListener("click", getMealRecipe);
+recipeShut.addEventListener("click", () => {
+  mealDetailsContent.parentElement.classList.remove("showRecipe");
+});
 
-
-function getRecipeList(){
-    let searchInput = document.getElementById 
-    ('search-inp').value.trim()
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = ""
-        if(data.meals){
-            data.meals.forEach(meal =>{
-                html += `<div class="recipe-item" data-id = "${meal.idMeal}">
-                        <div class="recipe-img">
-                            <img src="${meal.strMealThumb}" alt="lasagna">
+function getRecipeList() {
+  let searchInputTxt = document.getElementById("search-inp").value.trim();
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          html += `
+                    <div class = "recipe-item" data-id = "${meal.idMeal}">
+                        <div class = "recipe-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
                         </div>
-                        <div class="recipe-name">
+                        <div class = "recipe-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href="#" class="ingredients-btn">Ingredients</a>
+                            <a href = "#" class = "ingredients-btn">Ingredients</a>
                         </div>
-                    </div>`;
-            })
-        } else {
-            html = "Try again, meal wasn't found!"
-            mealOR.classList.add('error')
-        }
-        mealOR.innerHTML = html;
-    })
+                    </div>
+                `;
+        });
+        mealOr.classList.remove("error");
+      } else {
+        html = "Try again, meal wasn't found!";
+        mealOr.classList.add("error");
+      }
 
+      mealOr.innerHTML = html;
+    });
 }
 
-function getMealRecipe(e){
-    e.preventDefault();
-    if(e.target.classList.contains("recipe-btn")) {
+function getMealRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("ingredients-btn")) {
     let mealItem = e.target.parentElement.parentElement;
     fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=$
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => mealRecipeModal(data.meals));
+  }
+}
+
+function mealRecipeModal(meal) {
+  console.log(meal);
+  meal = meal[0];
+  let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+  mealDetailsContent.innerHTML = html;
+  mealDetailsContent.parentElement.classList.add("showRecipe");
 }
